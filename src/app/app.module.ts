9705b2b205ce58a9933fc9e0e6c3ae67ca501a3f
Aljosa Janjic <aljosa.janjic@ibm.com>
2023-05-29 11:42:40 +0200

    Restructuring imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -10,11 +11,9 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import {DropdownDirective} from './shared/dropdown.directive';
-import { FormsModule } from '@angular/forms';
+
 import { ShoppingListService } from './shopping-list/shopping-list.service'
-
-
+import {DropdownDirective} from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
