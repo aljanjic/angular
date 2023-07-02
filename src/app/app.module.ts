2aaa2b6df5cbecff85cc5b22f01bab9b11acb769
aljanjic <janjic_aljosa@hotmail.com>
2023-07-02 12:49:01 +0200

    Storing Recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -34,10 +35,10 @@ import {DropdownDirective} from './shared/dropdown.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
-    
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule 
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
