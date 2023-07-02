36e2d764df2c34eef3a3b0ae39300c5e02ae6701
aljanjic <janjic_aljosa@hotmail.com>
2023-07-02 14:56:49 +0200

    Resolving data before loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { SelectRecipeComponent } from "./recipes/select-recipe/select-recipe.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipesResolverSerice } from "./recipes/recipes-resolver.service";
 
 
 const appRoutes: Routes = [
@@ -12,8 +13,8 @@ const appRoutes: Routes = [
   {path: 'recipes', component: RecipesComponent, children:[
     { path:'', component: SelectRecipeComponent},
     { path:'new', component: RecipeEditComponent },
-    { path:':id', component: RecipeDetailComponent },
-    { path:':id/edit', component: RecipeEditComponent }
+    { path:':id', component: RecipeDetailComponent, resolve: [RecipesResolverSerice] },
+    { path:':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverSerice]}
   ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ];
